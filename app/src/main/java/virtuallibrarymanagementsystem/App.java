3092c5386f921ff3c7f3d120bf41e1a885f39cfd
Krishnanand B <kbandame@gmail.com>
2023-11-21 07:14:12 +0000

    Updated greeting

--- app/src/main/java/virtuallibrarymanagementsystem/App.java
+++ app/src/main/java/virtuallibrarymanagementsystem/App.java
@@ -5,7 +5,7 @@ package virtuallibrarymanagementsystem;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello User!";
     }
 
     public static void main(String[] args) {
